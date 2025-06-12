9a8388ca1e204e22a5fe3cf7ccec7aa0b425d378
Sonali-Malavi <sonaliymalavi@gmail.com>
2025-06-12 09:58:14 +0530

    eight commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -34,7 +34,7 @@ const ContactPage = () => {
 
                 <h2>Find Us on the Map</h2>
                 <p>[You can embed a Google Map here later using an iframe or a React map library.]</p>
-                {/* map add */}
+                <a href="https://maps.google.com/?q=Vivekanand+College" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
             </div>
             </div>
             </div>
